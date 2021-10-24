d2d28f74a0d024a6358a916047256617870ec41e
d3fau1t <aciddust20@gmail.com>
2021-10-24 14:10:29 +0900

    WIP

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from os import environ
+from datetime import timedelta
 
 from utils.env import init_json_env
 from utils.timer import get_datetime, calc_uptime
 from database import Mongo
+from authlib.integrations.flask_client import OAuth
 
 from flask import Flask, jsonify, render_template, request
 from flask_restx import Api, Resource
@@ -18,8 +20,24 @@ wokeup = get_datetime()
 api = Api()
 app = Flask(__name__)
 app.secret_key = "mealboard-secret-key"
-api.init_app(app, title="Wokeup API", version="1.0", description="Wokeup API")
+app.config["SESSION_COOKIE_NAME"] = "google-login-session"
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=5)
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+oauth = OAuth(app)
+google = oauth.register(
+    name="google",
+    client_id=environ.get("GOOGLE_CLIENT_ID"),
+    client_secret=environ.get("GOOGLE_CLIENT_SECRET"),
+    access_token_url="https://accounts.google.com/o/oauth2/token",
+    access_token_params=None,
+    authorize_url="https://accounts.google.com/o/oauth2/auth",
+    authorize_params=None,
+    api_base_url="https://www.googleapis.com/oauth2/v1/",
+    userinfo_endpoint="https://openidconnect.googleapis.com/v1/userinfo",  # This is only needed if using openId to fetch user info
+    client_kwargs={"scope": "openid email profile"},
+)
+
+api.init_app(app, title="Wokeup API", version="1.0", description="Wokeup API")
 api.add_namespace(Home, "/home")
 api.add_namespace(School, "/school")
 api.add_namespace(Meal, "/meal")
--- apps/user.py
+++ apps/user.py
@@ -1,11 +1,20 @@
 import os
 import json
-from flask import render_template, make_response, redirect, session, abort, request
+from flask import (
+    render_template,
+    make_response,
+    redirect,
+    session,
+    abort,
+    request,
+    url_for,
+)
 from flask_restx import Resource, Namespace
-
-from google.oauth2 import id_token
-from google_auth_oauthlib.flow import Flow
 import pathlib
+from authlib.integrations.flask_client import OAuth
+from functools import wraps
+from app import oauth
+
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
@@ -16,72 +25,35 @@ client_secrets_file = os.path.join(
 User = Namespace("User")
 
 
-flow = Flow.from_client_secrets_file(
-    client_secrets_file=client_secrets_file,
-    scopes=[
-        "https://www.googleapis.com/auth/userinfo.profile",
-        "https://www.googleapis.com/auth/userinfo.email",
-        "openid",
-    ],
-    redirect_uri="http://127.0.0.1:5000/user/callback",
-)
+def login_is_required(function):
+    @wraps(function)
+    def decorated_function(*args, **kwargs):
+        user = dict(session).get("profile", None)
+        if user:
+            return f(*args, **kwargs)
+        return redirect("/user/login")
 
-
-def login_is_required(func):
-    def wrapper(*args, **kwargs):
-        if "google_id" not in session:
-            return abort(401)
-        else:
-            return func()
-
-    return wrapper
+    return decorated_function
 
 
 @User.route("/login")
 class UserLogin(Resource):
     def get(self):
-        authorizational_url, state = flow.authorization_url()
-        session["state"] = state
-
-        # if u wanna render template uncomment this block
-        # headers = {"Content-Type": "text/html"}
-        # return make_response(render_template("login.html", test="hello"), 200, headers)
-
-        return redirect(authorizational_url, code=302)
+        google = oauth.create_client("google")
+        redirect_uri = url_for("/user/authorize", _external=True)
+        return google.authorize_redirect(redirect_uri)
 
 
-@User.route("/callback")
-class UserLoginCallback(Resource):
+@User.route("/authorize")
+class UserAuthorize(Resource):
     def get(self):
-        flow.fetch_token(authorization_response=request.url)
-
-        if not session["state"] == request.args["state"]:
-            abort(500)
-
-        credentials = flow.credentials
-        request_session = request.session()
-        cached_session = cachecontrol.CacheControl(request_session)
-        token_request = google.auth.transport.requests.Request(cached_session)
-
-        # print(f"request >> {request}")
-        # print(f'state >> {request.args.get("state")}')
-        # if not session.get("state") == request.args.get("state"):
-        #     abort(500)  # State does not match!
-        # credentials = flow.credentials
-        # request_session = requests.session()
-        # cached_session = cachecontrol.CacheControl(request_session)
-        # token_request = google.auth.transport.requests.Request(session=cached_session)
-
-        # id_info = id_token.verify_oauth2_token(
-        #     id_token=credentials._id_token,
-        #     request=token_request,
-        #     audience=GOOGLE_CLIENT_ID,
-        # )
-
-        # session["google_id"] = id_info.get("sub")
-        # session["name"] = id_info.get("name")
-        # return redirect("/user/protected_area")
-        return {"hello": "world"}
+        google = oauth.create_client("google")
+        token = google.authorize_access_token()
+        resp = google.get("userinfo")
+        user_info = resp.json()
+        session["profile"] = user_info
+        session.permanent = True
+        return redirect(url_for("/user/protected_area"))
 
 
 @User.route("/logout")
