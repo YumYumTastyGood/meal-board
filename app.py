be10cde302008b68bee7e1f3bda24cd49e7686ed
d3fau1t <aciddust20@gmail.com>
2021-10-24 20:52:10 +0900

    remove unused code

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from apps import app
 from flask_cors import CORS
 
 if __name__ == "__main__":
-    CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+    CORS(app, resources={r"/*": {"origins": "*"}})
     app.run(debug=True, host="0.0.0.0", port=5000)
