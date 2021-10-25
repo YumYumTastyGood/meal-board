d70ac31724859b0e2b9baa9eb08b8c400d107599
d3fau1t <aciddust20@gmail.com>
2021-10-25 22:28:40 +0900

    fix bug, update cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from apps import app
+from os import urandom
+from flask_cors import CORS
 
 if __name__ == "__main__":
-    CORS(app, support_credentials=True)
     app.secret_key = urandom(12)
     app.config["SESSION_COOKIE_NAME"] = "google-login-session"
     app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=5)
+    CORS(app, support_credentials=True)
     app.run(host="0.0.0.0", port=5000, debug=True)
