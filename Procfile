185ca9902cdde87fd0f3adc4be262c769731ff6e
d3fau1t <aciddust20@gmail.com>
2021-10-25 21:07:58 +0900

    config app.run

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app --bind 0.0.0.0:${PORT}
+web: FLASK_APP=app.py flask run
--- app.py
+++ app.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     app.secret_key = urandom(12)
     app.config["SESSION_COOKIE_NAME"] = "google-login-session"
     app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=5)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
