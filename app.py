92a68b696ca24ab64af27ceefe4311fb87e3f9f1
d3fau1t <aciddust20@gmail.com>
2021-10-25 23:40:02 +0900

    test cors

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
 from apps import app
-from os import urandom
-from flask_cors import CORS
 
 if __name__ == "__main__":
-    app.secret_key = urandom(12)
-    app.config["SESSION_COOKIE_NAME"] = "google-login-session"
-    app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=5)
-    CORS(app, support_credentials=True)
     app.run(host="0.0.0.0", port=5000, debug=True)
--- apps/__init__.py
+++ apps/__init__.py
@@ -1,11 +1,14 @@
-from os import environ, urandom
+from os import urandom
 from datetime import timedelta
 from flask_cors import CORS
 from flask import Flask
 from . import home, meal, misc, school
 
 app = Flask(__name__)
-
+app.secret_key = urandom(12)
+app.config["SESSION_COOKIE_NAME"] = "google-login-session"
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=5)
+CORS(app, support_credentials=True)
 app.register_blueprint(home.home)
 app.register_blueprint(meal.meal)
 app.register_blueprint(misc.misc)
