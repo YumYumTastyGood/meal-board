5d9f5c85c80b0aad804c99105c03f309d4956462
d3fau1t <aciddust20@gmail.com>
2021-10-28 22:17:45 +0900

    :seedling: Add google login
    done

--- apps/home.py
+++ apps/home.py
@@ -1,5 +1,5 @@
 import json
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, session, request, redirect
 
 home = Blueprint("home", __name__, url_prefix="/")
 
@@ -13,3 +13,35 @@ def get_home():
         "home.html",
         hello="Greeting",
     )
+
+
+@home.route("/login")
+def get_login():
+    from apps import google
+
+    return google.authorize(callback=url_for("home.callback", _external=True))
+
+
+@home.route("/logout")
+def get_logout():
+    session.pop("google_token", None)
+    return redirect(url_for("home.get_home"))
+
+
+@home.route("/login/callback")
+def callback():
+    from apps import google
+
+    @google.tokengetter
+    def get_google_oauth_token():
+        return session.get("google_token")
+
+    resp = google.authorized_response()
+    if resp is None:
+        return "Access denied: reason=%s error=%s" % (
+            request.args["error"],
+            request.args["error_description"],
+        )
+    session["google_token"] = (resp["access_token"], "")
+    me = google.get("userinfo")
+    return {"data": me.data}
